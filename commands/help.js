b6e2ad5d99e16d14caaa6f6722463bcae9d9685e
Milkry <panosthathop@gmail.com>
2022-02-24 01:24:38 +0000

    Updated help menu

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,20 @@ module.exports = {
     description: 'Displays a help menu',
     run: async (client, message, args) => {
         const access = [client.config.myId];
+        const helpDescription = `
+            Prefix: [ **.** ]
+            \n> rickroll {channelId}
+            \n> dmrr {@user}
+            \n> disconnect
+            \n> reload {command name}
+            \n> nh {code} {optional: -s}`;
         if (!access.includes(message.author.id)) return;
 
         message.delete();
         const helpMenu = new MessageEmbed()
             .setColor('#4500d9')
             .setTitle('Help Menu')
-            .setDescription("Prefix: [ **.** ]\n\n.rickroll {channelId}\n.dmrr {@user}\n.disconnect\n.reload {command name}")
+            .setDescription(helpDescription)
             .setTimestamp()
         message.author.send({ embeds: [helpMenu] });
     }
