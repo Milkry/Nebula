48761e82ec675689879d88a125fa81fe826da51b
Milkry <panosthathop@gmail.com>
2022-06-07 20:15:15 +0100

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -17,8 +17,13 @@ module.exports = {
             **rel/reload** *{full_command_name}* : Reloads a specified command
             **nh/nhentai** *{code} [-s : sends as a dm]* : Responds with details about an nHentai code
             **mon/monitor** *{(s)tatus}* : Displays a list of monitored voice channels
+            **mon/monitor** *{(s)tatus} {channel_id}* : Displays more details about a specific monitored channel
             **mon/monitor** *{(g)lobal} {on/off}* : Enables/Disables global monitoring for the server
-            **mon/monitor** *{channel_id/channel_name} {on/off}* : Enables/Disables monitoring for a specific channel
+            **mon/monitor** *-c {channel_id/channel_name} {on/off}* : Enables/Disables monitoring for a specific channel
+            **mon/monitor** *add {channel_id} [@user]* : Adds a user in the access list of a specific monitored channel (Adds channel too if not monitored)
+            **mon/monitor** *add {channel_id}* : Adds the channel to the monitor list
+            **mon/monitor** *remove {channel_id} [@user]* : Removes a user from the access list of a specific monitored channel
+            **mon/monitor** *remove {channel_id}* : Removes the channel from the monitor list
 
             **Note:** You can get the channel id by enabling **Developer Mode** in *Advanced Settings* and right-clicking a voice channel.
         `;
