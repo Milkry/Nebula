585ec1167b27ec0baa69506ea54ca671b94a3bec
Milkry <panosthathop@gmail.com>
2022-06-11 14:49:58 +0100

    Improved help menu

--- commands/help.js
+++ commands/help.js
@@ -10,42 +10,40 @@ module.exports = {
     cooldown: 0,
     run: async (client, message, args) => {
         try {
-            var helpDescription = "";
-            var ownerHelp = "";
+            var helpDescription = ``;
 
-            helpDescription = `
-            Prefix: [ **.** ]
-
-            **Note:** You can get the channel id by enabling **Developer Mode** in *Advanced Settings* and right-clicking a voice channel.
+            helpDescription += `
+                Prefix: [ **.** ]
             
-            **nh/nhentai** *{nh_code} [-p : post to channel]* : Responds with details about an nHentai code either privately or publically
-            **nh/nhentai** *{nh_code} {@user}* : DM's the user details about an nHentai code
-            **server** : Displays information about the server
-
-        `;
+                🞛 **nh/nhentai <nh_code> [-p : respond to channel]** : Responds with details about a doujin
+                🞛 **nh/nhentai <nh_code> <@user>** : DM's the user details about a doujin
+                🞛 **server** : Displays information about the server
+            `;
 
             if (message.author.id === process.env.OWNER_ID) {
-                ownerHelp = `
-                **rr/rickroll** *{channel_id}* : Bot joins the voice channel and starts playing rick roll
-                **dmrr/dmrickroll** *{@user}* : DMs the specified user a little surprise :)
-                **disc/disconnect** : Disconnects the bot from the voice channel
-                **rel/reload** *{full_command_name}* : Reloads a specified command
-                **mon/monitor** *{(s)tatus}* : Displays a list of monitored voice channels
-                **mon/monitor** *{(s)tatus} {channel_id}* : Displays more details about a specific monitored channel
-                **mon/monitor** *{(g)lobal} {on/off}* : Enables/Disables global monitoring for the server
-                **mon/monitor** *-c {channel_id/channel_name} {on/off}* : Enables/Disables monitoring for a specific channel
-                **mon/monitor** *add {channel_id} [@user]* : Adds a user in the access list of a specific monitored channel (Adds channel too if not monitored)
-                **mon/monitor** *add {channel_id}* : Adds the channel to the monitor list
-                **mon/monitor** *remove {channel_id} [@user]* : Removes a user from the access list of a specific monitored channel
-                **mon/monitor** *remove {channel_id}* : Removes the channel from the monitor list
-            `;
+                helpDescription += `
+                    🞛 **rr/rickroll <channel_id>** : Bot joins the voice channel and starts playing rick roll
+                    🞛 **dmrr/dmrickroll <@user>** : DMs the specified user a little surprise :)
+                    🞛 **disc/disconnect** : Disconnects the bot from the voice channel
+                    🞛 **rel/reload <full_command_name>** : Reloads a specified command
+                    🞛 **mon/monitor <(s)tatus>** : Displays a list of monitored voice channels
+                    🞛 **mon/monitor <(s)tatus> <channel_id>** : Displays more details about a specific monitored channel
+                    🞛 **mon/monitor <(g)lobal> <on/off>** : Enables/Disables global monitoring for the server
+                    🞛 **mon/monitor -c <channel_id/channel_name> <on/off>** : Enables/Disables monitoring for a specific channel
+                    🞛 **mon/monitor add <channel_id> <@user>** : Adds a user in the access list of a specific monitored channel (Adds channel too if not monitored)
+                    🞛 **mon/monitor add <channel_id>** : Adds the channel to the monitor list
+                    🞛 **mon/monitor remove <channel_id> <@user>** : Removes a user from the access list of a specific monitored channel
+                    🞛 **mon/monitor remove <channel_id>** : Removes the channel from the monitor list
+                `;
+                helpDescription += `\n**Note**: You can get the channel id by enabling __Developer Mode__ in *Advanced Settings* and right-clicking on a voice channel.`;
             }
 
             await message.delete();
             const helpMenu = new MessageEmbed()
                 .setColor(client.theme.Notification)
-                .setTitle('Help Menu')
-                .setDescription(helpDescription + ownerHelp)
+                .setAuthor({ name: `Nebula`, iconURL: client.user.avatarURL({ dynamic: true }) })
+                .setTitle(`:scroll: **HELP MENU** :scroll:`)
+                .setDescription(helpDescription)
             message.author.send({ embeds: [helpMenu] });
         }
         catch (e) {
